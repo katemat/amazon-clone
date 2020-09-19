6955e6c6d5c3dcbb45d19a41c9a04b875ebefaea
Katerina <matveevakaterinka@gmail.com>
2020-09-19 15:38:30 +1000

    product page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,12 +12,49 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          <Product />
+          <Product
+            title="The Barefoot Investor 2019 Update: The Only Money Guide You'll Ever Need"
+            price={19.99}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51gksI6yl0L._SX378_BO1,204,203,200_.jpg"
+          />
+          <Product
+            title="Kenwood kMix Stand Mixer, 1000 W, Black"
+            price={538.76}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/91gRKbX%2BS8L._AC_SX522_.jpg"
+          />
         </div>
 
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product
+            title="LEGO Super Mario Adventures with Mario Starter Course 71360 Building Kit"
+            price={59.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/91YDrKVKjHL._AC_SX679_.jpg"
+          />
+          <Product
+            title="Wireless Earbuds, TaoTronics Bluetooth Headphones Smart AI Noise Reduction Technology for Clear Calls"
+            price={56.99}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/717fORr2gdL._AC_SY606_.jpg"
+          />
+          <Product
+            title="ASAKUKI Aromatherapy Essential Oils"
+            price={25.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/817q0P4-n1L._AC_SX679_.jpg"
+          />
+        </div>
 
-        <div className="home__row"></div>
+        <div className="home__row">
+          <Product
+            title="Panasonic Touch Screen LUMIX DMCG7 4K Camera + 14-42mm F3.5-5.6 + 45-150mm F4.0-5.6 Lens, Black (DMC-G7WGN-K)"
+            price={930.0}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/81aDrCqnv8L._AC_SX569_.jpg"
+          />
+        </div>
       </div>
     </div>
   )
